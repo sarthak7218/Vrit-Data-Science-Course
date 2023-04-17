a63451a6c0c1163abb242eda69b8dcb0ca02920b
sarthak <sarthakbhattarai1997@gmail.com>
2023-04-17 12:21:57 +0545

    added tuple file

---
+++ sarthak.py
@@ -0,0 +1,13 @@
+age=12
+name = "John"
+print("My name is {} and I am {} years old.". format(name,age) )
+
+# f-string
+print(f"My name is {name} and I am {age} years old")
+
+a = "Hello"
+print (a.replace("l", "o"))
+
+b = 'Hello!'
+b.strip('!')
+print (b.split('e'))
